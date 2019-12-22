5bce140e9bb9ba670c5be0aa059d7f732e8fa9f5
fecoramirez <fecoramirez2@gmail.com>
2019-12-21 19:11:49 -0500

    Showroom Implementado

---
+++ Assets/CambioDia.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioDia : MonoBehaviour
+{
+    public GameObject dia;
+    public GameObject noche;
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag=="Player") {
+            dia.SetActive(false);
+            noche.SetActive(true);
+        }
+    }
+
+}
